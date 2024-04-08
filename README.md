00105a8d99846bf05915e1f3128363b230ebdfbe
Marcos <marcos@Marcoss-MacBook-Pro.local>
2024-04-08 08:55:36 -0300

    finished exercise one
    refactored verificar_melhor_dia_da_semana function and improved code organization
    refactored check_best_week_day function to handle tie case
    updated readme to fix typos

--- README.md
+++ README.md
@@ -1,52 +1,75 @@
 # fiap-phase2-chapter3-activity
-This repository contains an activity for Phase 2, Chapter 3 of the Software Analysis and Development course. The activity consists of four problems that are designed to help students practice using loops to solve programming challenges.
+This repository contains an activity for Phase 2, Chapter 3 of the Software Analysis and Development course. 
+The activity consists of four problems that are designed to help students practice using loops to solve 
+programming challenges.
 
 ## Problems
 ### Problem 1
-A Bidu é uma startup na área de Fintech fundada em 2011 que ajuda os usuários a controlar suas fontes de receitas, gastos, dívidas e investimentos. Ela precisa realizar uma votação para escolher qual dia da semana é o melhor para a realização das lives com o time da mentoria financeira. Desenvolva um programa em que os colaboradores informem um dos 5 dias da semana (segunda-feira, terça-feira, quarta-feira, quinta-feira e sexta-feira) da sua preferência para participar da live. Verifique e exiba ao final, qual dia foi o escolhido pelos colaboradores.
-Observação: Verifique o número de colaboradores que irão participar da votação para programar sua estrutura de repetição.
+A Bidu é uma startup na área de Fintech fundada em 2011 que ajuda os usuários a controlar suas fontes de 
+receitas, gastos, dívidas e investimentos. Ela precisa realizar uma votação para escolher qual dia da semana 
+é o melhor para a realização das lives com o time da mentoria financeira. Desenvolva um programa em que 
+os colaboradores informem um dos 5 dias da semana (segunda-feira, terça-feira, quarta-feira, 
+quinta-feira e sexta-feira) da sua preferência para participar da live. 
+Verifique e exiba ao final, qual dia foi o escolhido pelos colaboradores.  
+
+Observação: Verifique o número de colaboradores que irão participar da votação para programar sua estrutura 
+de repetição.
 
 ### Problem 2
-A compra de um veículo pode ser realizada parcelada. Crie um programa que receba o valor de um carro e mostre uma tabela com os seguintes dados: preço final, quantidade de parcelas e valor da parcela. Considere o seguinte:  
+A compra de um veículo pode ser realizada parcelada. Crie um programa que receba o valor de um carro e 
+mostre uma tabela com os seguintes dados: preço final, quantidade de parcelas e valor da parcela. 
+Considere o seguinte:  
 a) O preço final para compra à vista tem um desconto de 20%:  
-b) A quantidade de parcelas pode ser 6, 12, 18, 24, 30, 36, 42, 48, 54 e 60:
-Os percentuais de acréscimo seguem na tabela abaixo:  
-Quantidade de Parcelas	Percentual de Acréscimo sobre o preço final  
-6	3%  
-12	6%  
-18	9%  
-24	12%  
-30	15%  
-36	18%  
-42	21%  
-48	24%  
-54	27%  
-60	30%  
+b) A quantidade de parcelas pode ser 6, 12, 18, 24, 30, 36, 42, 48, 54 e 60:  
 
-Observação: Na saída do programa, utilize estrutura de repetição para apresentar a listagem, conforme o modelo acima.
+Os percentuais de acréscimo seguem na tabela abaixo:  
+Quantidade de Parcelas | Percentual de Acréscimo sobre o preço final  
+6 | 3%  
+12 | 6%  
+18 | 9%  
+24 | 12%  
+30 | 15%  
+36 | 18%  
+42 | 21%  
+48 | 24%  
+54 | 27%  
+60 | 30%  
+
+Observação: Na saída do programa, utilize estrutura de repetição para apresentar a listagem, 
+conforme o modelo acima.
 
 ### Problem 3
-Na oferta de um produto de crédito aos clientes, três informações são muito importantes apresentar ao cliente: valor da dívida, a taxa de juros e o número de parcelas para pagamento do empréstimo contraído junto à Fintech. Faça um programa que receba o valor de uma dívida e mostre uma tabela com os seguintes dados:  
+Na oferta de um produto de crédito aos clientes, três informações são muito importantes apresentar ao 
+cliente: valor da dívida, a taxa de juros e o número de parcelas para pagamento do empréstimo contraído 
+junto à Fintech. Faça um programa que receba o valor de uma dívida e mostre uma tabela com os seguintes dados:  
 Valor da dívida, valor do juro, quantidade de parcelas e valor da parcela.  
 
 Os juros e a quantidade de parcelas seguem a tabela:  
-Quantidade de Parcelas | % de juros sobre o valor inicial da dívida
+Quantidade de Parcelas | % de juros sobre o valor inicial da dívida  
 1 | 0  
 3 | 10  
 6 | 15  
 9 | 20  
 12 | 25  
 
-Observação: Na saída do programa, utilize estrutura de repetição para apresentar a listagem, conforme o modelo acima.
+Observação: Na saída do programa, utilize estrutura de repetição para apresentar a listagem, 
+conforme o modelo acima.
 
 ### Problem 4
-4- Toda vez que um cliente realiza um resgate de uma aplicação financeira, o sistema deve calcular a alíquota de imposto de renda (IR) que deve ser aplicada sobre aquele resgate, considerando o número de dias que o valor permaneceu aplicado, conforme a tabela abaixo:  
+Toda vez que um cliente realiza um resgate de uma aplicação financeira, o sistema deve calcular a alíquota 
+de imposto de renda (IR) que deve ser aplicada sobre aquele resgate, considerando o número de dias que 
+o valor permaneceu aplicado, conforme a tabela abaixo:  
 
 Até 180 dias = alíquota de 22,5% de IR.  
 De 181 a 360 dias = alíquota de 20% de IR.  
 De 361 a 720 dias = alíquota de 17,5% de IR.  
 Acima de 720 dias = alíquota de 15% de IR.  
 
-É o que acontece, por exemplo, com o CDB - Certificado de Depósito Bancário, uma aplicação de renda fixa comumente oferecida pelas Fintechs. Outros investimentos em renda fixa, como LCI e LCA, respectivamente, Letra de Crédito Imobiliário e Letra de Crédito do Agronegócio são isentos de imposto de renda. Escreva um programa que receba o tipo de investimento do qual se deseja realizar um resgate (1 para CDB, 2 para LCI e 3 para LCA), o valor a ser resgatado e o número de dias que esse valor permaneceu investido e, se for o caso, calcule o valor referente ao imposto de renda.
+É o que acontece, por exemplo, com o CDB - Certificado de Depósito Bancário, uma aplicação de renda fixa 
+comumente oferecida pelas Fintechs. Outros investimentos em renda fixa, como LCI e LCA, respectivamente, 
+Letra de Crédito Imobiliário e Letra de Crédito do Agronegócio são isentos de imposto de renda. 
+Escreva um programa que receba o tipo de investimento do qual se deseja realizar 
+um resgate (1 para CDB, 2 para LCI e 3 para LCA), o valor a ser resgatado e o número de dias que esse 
+valor permaneceu investido e, se for o caso, calcule o valor referente ao imposto de renda.
 
 Atenção! O programa deve consistir se o investimento fornecido é válido, ou seja, 1, 2 o 3.
--- RM94562_EX01.py
+++ RM94562_EX01.py
@@ -12,95 +12,139 @@
 # qual dia da semana é o melhor para realizar live
 #
 # verificar número de colaboradores
-# colaborador informa um dia da semana. Formato: (xxxx)-(feira)
+# colaborador informa um dia da semana. Formato: (xxxx)-feira
 # verificar e exibir qual o dia escolhido
 
-mensagem_boas_vindas = 'boas vindas'
-mensagem_informe_dia = 'informe dia: '
-mensagem_informe_quantidade_colaboradores = 'colaboradores: '
-mensagem_erro_informe_dia = 'erro dia'
-mensagem_erro_quantidade_colaboradores = 'erro colaboradores'
-dias_da_semana_validos = ['seg', 'ter', 'qua']
-dias_da_semana_informados = []
+welcome_message = ('Bem-vindo(a) à votação da Bidu para escolher o melhor dia da semana para a realização '
+                   'das lives com o time da mentoria financeira!\n')
+message_inform_number_of_collaborators = 'Quantos colaboradores vão participar da votação? '
+message_error_number_of_collaborators = 'Valor inválido. Por favor, informe um número inteiro positivo.\n'
+valid_week_days = ['segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira']
+
+
+def get_user_input(message: str) -> int:
+    while True:
+        number_of_collaborators = input(message)
+        if number_of_collaborators.isdigit():
+            is_zero = int(number_of_collaborators) == 0
+
+            if not is_zero:
+                return int(number_of_collaborators)
+            else:
+                print(message_error_number_of_collaborators)
+                continue
+        else:
+            print(message_error_number_of_collaborators)
+            continue
+
+
+def get_collaborators_votes(number_of_collaborators: int) -> list:
+    preferred_week_days_selected = []
+
+    for index in range(number_of_collaborators):
+        preferred_week_day = ''
+        collaborator_number = index + 1
+        while preferred_week_day not in valid_week_days:
+            preferred_week_day = input(f'\nColaborador {collaborator_number}, por favor informe seu dia preferido da '
+                                       f'semana para a live ({', '.join(valid_week_days)}): ').strip().lower()
+            if preferred_week_day not in valid_week_days:
+                print(f'Dia inválido. Por favor insira um dos seguintes dias: {', '.join(valid_week_days)}.')
+            else:
+                print('Obrigado pelo seu voto.')
+                if collaborator_number < number_of_collaborators:
+                    print('Próximo colaborador!')
+
+        preferred_week_days_selected.append(preferred_week_day)
+    else:
+        print('\nFim da votação. Realizando a contagem...\n')
+
+    return preferred_week_days_selected
+
+
+def get_preferred_week_day_or_days(preferred_week_days_selected: list) -> list:
+    greatest_number_of_occurrences = 0
+    already_verified_week_days = []
+    most_voted_week_days = []
+
+    for week_day in preferred_week_days_selected:
+        if week_day in already_verified_week_days:
+            continue
+
+        already_verified_week_days.append(week_day)
+        occurrences = preferred_week_days_selected.count(week_day)
+
+        if occurrences >= greatest_number_of_occurrences:
+            previous_greatest_number_of_occurrences = greatest_number_of_occurrences
+            greatest_number_of_occurrences = occurrences
+
+            if greatest_number_of_occurrences > previous_greatest_number_of_occurrences:
+                most_voted_week_days[:] = []
+                most_voted_week_days.append(week_day)
+            else:
+                most_voted_week_days.append(week_day)
+
+    return most_voted_week_days
+
+
+# note: liked this solution because you can find out the vote for each collaborator. not possible in
+# check_best_week_day(). don't like it because it takes too much time and has unnecessary complexity, like
+# two for loops for example
+def verificar_melhor_dia_da_semana():
+    print(welcome_message)
+
+    number_of_collaborators = get_user_input(message_inform_number_of_collaborators)
+    print(f'Ok. A votação será realizada com {number_of_collaborators} colaboradores. '
+          f'Preparando para coletar os votos...')
+
+    preferred_week_days_selected = get_collaborators_votes(number_of_collaborators)
+
+    most_voted_week_day_or_days = get_preferred_week_day_or_days(preferred_week_days_selected)
+
+    if len(most_voted_week_day_or_days) == 1:
+        print(f'O melhor dia para a live, de acordo com os colaboradores, é {most_voted_week_day_or_days[0]}.')
+    else:
+        print(f'Houve um empate para o melhor dia para a live, de acordo com os colaboradores. Os seguintes '
+              f'dias receberam mais votos: {', '.join(most_voted_week_day_or_days)}.')
+
+
+def check_best_week_day():
+    # Create an empty dictionary to store the votes for each day of the week
+    votes = {'segunda-feira': 0, 'terça-feira': 0, 'quarta-feira': 0, 'quinta-feira': 0, 'sexta-feira': 0}
+
+    # Prompt the user to enter the number of collaborators who will participate in the vote
+    num_collaborators = int(input("How many collaborators will participate in the vote? "))
+
+    # Loop through each collaborator and prompt them to enter their preferred day of the week for the live event
+    for i in range(num_collaborators):
+        while True:
+            try:
+                preferred_day = input(
+                    f"Collaborator {i + 1}, please enter your preferred day of the week for the live event ("
+                    f"segunda-feira, terça-feira, quarta-feira, quinta-feira, sexta-feira): ")
+                if preferred_day not in votes:
+                    raise ValueError
+                votes[preferred_day] += 1
+                break
+            except ValueError:
+                print(
+                    "Invalid day of the week. Please enter one of the following: segunda-feira, terça-feira, "
+                    "quarta-feira, quinta-feira, sexta-feira")
+
+    # Find the day of the week with the most votes
+    max_votes = max(votes.values())
+    best_day = [day for day, votes in votes.items() if votes == max_votes]
+
+    # Print the best day(s) for the live event, according to the collaborators
+    if len(best_day) == 1:
+        print(f"The best day for the live event, according to the collaborators, is {best_day[0]}.")
+    else:
+        print(
+            f"There was a tie for the best day for the live event, according to the collaborators. The following "
+            f"days received the most votes: {', '.join(best_day)}.")
 
 
 def main():
     verificar_melhor_dia_da_semana()
-    # check_best_week_day()
-
-
-def verificar_melhor_dia_da_semana():
-    numero_de_colaboradores = input(mensagem_informe_quantidade_colaboradores)
-
-    if numero_de_colaboradores.isdigit():
-        if int(numero_de_colaboradores) == 0:
-            print(mensagem_erro_quantidade_colaboradores)
-            verificar_melhor_dia_da_semana()
-
-        else:
-            print('numero_de_colaboradores: ', numero_de_colaboradores)
-
-            for colaborador in range(int(numero_de_colaboradores)):
-                dia_informado = ''
-                while dia_informado not in dias_da_semana_validos:
-                    dia_informado = input(f'colaborador {colaborador + 1}, ' + mensagem_informe_dia)
-                    print('dia_informado: ', dia_informado)
-
-                dias_da_semana_informados.append(dia_informado)
-            else:
-                print(f'dias_da_semana_informados: {dias_da_semana_informados}')
-
-            maior_numero_de_ocorrencias = 0
-            dia_da_semana_selecionado = ''
-            dias_da_semana_verificados = []
-            for dia in dias_da_semana_informados:
-                print('dia: ', dia)
-                if dia in dias_da_semana_verificados:
-                    continue
-
-                dias_da_semana_verificados.append(dia)
-                ocorrencias = dias_da_semana_informados.count(dia)
-                print('ocorrencias: ', ocorrencias)
-                if ocorrencias > maior_numero_de_ocorrencias:
-                    maior_numero_de_ocorrencias = ocorrencias
-                    dia_da_semana_selecionado = dia
-                    print('maior_numero_de_ocorrencias: ', maior_numero_de_ocorrencias)
-                    print('dia_da_semana_selecionado: ', dia_da_semana_selecionado)
-
-            else:
-                print('dias_da_semana_verificados: ', dias_da_semana_verificados)
-                print('dia_da_semana_selecionado: ', dia_da_semana_selecionado)
-
-    else:
-        print(mensagem_erro_quantidade_colaboradores)
-        verificar_melhor_dia_da_semana()
-
-
-def check_best_week_day():
-    # Create an empty dictionary to store the votes
-    votes = {'segunda-feira': 0, 'terça-feira': 0, 'quarta-feira': 0, 'quinta-feira': 0, 'sexta-feira': 0}
-
-    # Ask the user for the number of collaborators who will participate in the vote
-    num_collaborators = int(input('How many collaborators will participate in the vote? '))
-
-    # Use a for loop to iterate through each collaborator and ask for their preferred day
-    for i in range(num_collaborators):
-        preferred_day = input('Collaborator {}: Which day of the week do you prefer for the live event? '.format(i + 1))
-
-        # Check if the preferred day is valid, and if so, add a vote for that day
-        if preferred_day in votes:
-            votes[preferred_day] += 1
-        else:
-            print(
-                'Invalid day of the week. Please enter one of the following: segunda-feira, terça-feira, '
-                'quarta-feira, quinta-feira, sexta-feira')
-
-    # Find the day with the most votes
-    max_votes = max(votes.values())
-    best_day = [day for day, votes in votes.items() if votes == max_votes]
-
-    # Print the results
-    print('The best day(s) for the live event, according to the collaborators, is/are: {}'.format(', '.join(best_day)))
 
 
 if __name__ == '__main__':
